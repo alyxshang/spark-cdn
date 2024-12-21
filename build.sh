633498620caebd14772ae4ac572f469a513cedf5
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2024-12-21 19:18:29 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 # SPARK CDN by Alyx Shang.
 # Licensed under the FSL v1.
 
-buildJS(){
-	cd js
-	mkdir javascript
-	deno bundle navigation.ts >> javascript/navigation.js
- 	cd ..
-}
-
 buildCDN(){
 	mkdir cdn
 	mv css cdn
 	mv fonts cdn
 	mv images cdn
-	mv js/javascript cdn
 }
 
 main(){
